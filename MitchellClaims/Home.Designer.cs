ab828480c5183f010255ee4698b3213d82baceaa
Monish Ratanji <mratanji@ucsd.edu>
2015-04-10 05:05:27 -0700

    Commit for handling xml files with more than one entry

---
+++ MitchellClaims/Home.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace MitchellClaims
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addClaimButton = new System.Windows.Forms.Button();
+            this.viewClaimsButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addClaimButton
+            // 
+            this.addClaimButton.Location = new System.Drawing.Point(98, 58);
+            this.addClaimButton.Name = "addClaimButton";
+            this.addClaimButton.Size = new System.Drawing.Size(75, 23);
+            this.addClaimButton.TabIndex = 0;
+            this.addClaimButton.Text = "Add Claim";
+            this.addClaimButton.UseVisualStyleBackColor = true;
+            this.addClaimButton.Click += new System.EventHandler(this.addClaimButton_Click);
+            // 
+            // viewClaimsButton
+            // 
+            this.viewClaimsButton.Location = new System.Drawing.Point(98, 111);
+            this.viewClaimsButton.Name = "viewClaimsButton";
+            this.viewClaimsButton.Size = new System.Drawing.Size(75, 23);
+            this.viewClaimsButton.TabIndex = 1;
+            this.viewClaimsButton.Text = "View Claims";
+            this.viewClaimsButton.UseVisualStyleBackColor = true;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.viewClaimsButton);
+            this.Controls.Add(this.addClaimButton);
+            this.Name = "Home";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addClaimButton;
+        private System.Windows.Forms.Button viewClaimsButton;
+    }
+}
+
---
+++ MitchellClaims/Home.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+
+
+namespace MitchellClaims
+{
+    public partial class Home : Form
+    {
+        public HashSet<MitchellClaimType> claimsDB = new HashSet<MitchellClaimType>();
+        public Home()
+        {
+            // Already have compiled a library version of the XML schema        
+            InitializeComponent();
+        }
+
+        private void addClaimButton_Click(object sender, EventArgs e)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(MitchellClaimType));
+            FileStream fs = new FileStream("create-claim.xml", FileMode.Open);
+            List<MitchellClaimType> newClaim = ser.Deserialize(fs) as List<MitchellClaimType>;
+            if (newClaim != null)
+            {
+                // To keep track of how new entries are added
+                List<string> failed = new List<string>();
+                Boolean success = false;
+                int numAdded = 0;
+                int numFailed = 0;
+
+                // Loop through and add all the new claims
+                for (int i = 0; i < newClaim.Count; i++)
+                {
+                    success = claimsDB.Add(newClaim[i]);
+                    //Console.WriteLine("Claim #" + newClaim.ClaimNumber +" add status: " + result);
+                    //Console.WriteLine("Size of hashset: " + claimsDB.Count);
+                    //Console.WriteLine("hashcode: " + newClaim.GetHashCode());
+                    if (success)
+                    {
+                        numAdded++;
+                        Console.WriteLine("Added");
+                    }
+                    else
+                    {
+                        numFailed++;
+                        failed.Add(newClaim[i].ClaimNumber);
+                    }
+                    // For safety reasons, reset success to false
+                    success = false;
+                }
+                // Report the adding!
+                //MessageBox.Show(numAdded + " new claims added, " + numFailed + " claims failed.");
+                // Save the claim ID's for later
+            }
+            // Remember to close the file!
+            fs.Close();
+        }
+    }
+}
